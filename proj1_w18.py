cd4d7433a5c546514b72130c1409f2f78f7acbc0
Rebecca Penny <rebeccapenny@0587396334.wireless.umich.net>
2018-02-02 16:06:26 -0500

    trying to pull from json

--- proj1_w18.py
+++ proj1_w18.py
@@ -3,55 +3,81 @@ import json
 
 json_file = open('sample_json.json', 'r')
 data = json_file.read()
-json_list = json.loads(data)
+json_dict = json.loads(data)
 
 
 class Media:
 	def __init__(self, title="No Title", author="No Author", release_year = "N/A", json = None):
-		self.title = title
-		self.author = author
-		self.year = release_year
-		self.json = json
+		if json is None:
+			self.title = title
+			self.author = author
+			self.year = release_year
+			self.json = json
+		else:
+			self.process_json_dict(json_dict)
+
+	def process_json_dict(self, json_dict):
+		self.title = json_dict['trackName']
+		self.author = json_dict['artistName']
+		self.year = json_dict['releaseDate'][:4]
+
 
 	def __str__(self):
-		#return(self.title + " by " + self.author + " (" + str(self.year) + ")")
-		return(self.title + " by " + self.author + " (" + str(self.year) + ")")
+		return(self.title + " by " + self.author + " (" + (self.year) + ")")
 
 
 	def __len__(self):
 		return(0)
 
+
 class Song(Media):
 	def __init__(self, title= "No Title", author = "No Author", release_year ="N/A", album = "No Album", genre = "Unknown", track_length= "N/A", json = None):
-		super().__init__(title, author, release_year)
-		self.album = album
-		self.genre = genre
-		self.length = track_length
-		self.json = json
+		if json is None:
+			super().__init__(title, author, release_year)
+			self.album = album
+			self.genre = genre
+			self.length = track_length
+			self.json = json
+		else:
+			super().process_json_dict()
+			self.year = json_dict["releaseDate"][:4]
+            self.album = json_dict["collectionName"]
+            self.genre = json_dict["primaryGenreName"]
+            self.length = json_dict["trackTimeMillis"]
+
 
 	def __str__(self):
 		return(super().__str__() + "[" + self.genre + "]")
-		#return(self.title + " by " + self.author + "(" + self.year + ")" + " [" + self.genre + "]")
 
 	def __len__(self):
 		return(self.length)
+		# need to convert
+
 
 class Movies(Media):
 	def __init__(self, title= "No Title", author = "No Author", release_year = "N/A", rating = "N/A", movie_length = "N/A", json = None):
-		super().__init__(title, author, release_year)
-		self.rating = rating
-		self.movie_length = movie_length
-		self.json = json
+		if json is None:
+			super().__init__(title, author, release_year)
+			self.rating = rating
+			self.movie_length = movie_length
+			self.json = json
+		else:
+			super().process_json_dict()
+			self.rating = json_dict["contentAdvisoryRating"]
+            self.movie_length= json_dict["trackTimeMillis"]
 
 	def __str__(self):
 		return(super().__str__() + "[" + self.rating + "]")
-		#return(self.title + " by " + self.author + " (" + self.year + ")" + "[" + self.rating + "]")
 
 	def __len__(self):
 		return((int(self.movie_length) / 1000) / 60)
+	#might be wrong
 
 
-print(json_list)
+
+a = Media(json_dict)
+print(len(a))
+print(a.title)
 
 
 
